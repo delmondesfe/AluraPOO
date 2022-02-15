c8f17f18c15bce82e77a31b22c31e5c582f47372
delmondesfe <59212050+delmondesfe@users.noreply.github.com>
2022-02-14 21:25:13 -0300

    Commits

---
+++ 03-ByteBank/ContaCorrente.cs
@@ -0,0 +1,12 @@
+﻿namespace _03_ByteBank
+{
+    public class ContaCorrente
+    {
+
+        public string titular;
+        public int agencia = 863;
+        public int numero;
+        public double saldo = 100;
+
+    }
+}
